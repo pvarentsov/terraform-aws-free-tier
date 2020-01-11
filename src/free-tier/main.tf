e2121dcfce0ad5a3d36326ae51ebd4777766b447
pvarentsov <varentsovpavel@gmail.com>
2020-01-11 17:10:16 +0300

    Added internet_gateway, public_subnet, route_table modules

--- src/free-tier/main.tf
+++ src/free-tier/main.tf
@@ -10,3 +10,23 @@ terraform {
 module "vpc" {
   source = "../modules/vpc"
 }
+
+module "public_subnet" {
+  source = "../modules/public-subnet"
+
+  vpc_id = module.vpc.vpc_id
+}
+
+module "internet_gateway" {
+  source = "../modules/internet-gateway"
+
+  vpc_id = module.vpc.vpc_id
+}
+
+module "route_table" {
+  source = "../modules/route-table"
+
+  vpc_id              = module.vpc.vpc_id
+  internet_gateway_id = module.internet_gateway.internet_gateway_id
+  public_subnet_id    = module.public_subnet.public_subnet_id
+}
